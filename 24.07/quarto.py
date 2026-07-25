902f38cd19fb1c524ad030ae4c2f95463bf67064
tad-carlospereira2 <charleswinkler76@gmail.com>
2026-07-24 21:32:00 -0300

    Primeiro commit

---
+++ 24.07/quarto.py
@@ -0,0 +1,9 @@
+contador = 0
+
+def incrementar():
+    global contador
+    contador += 1
+
+incrementar()
+incrementar()
+print(contador)
---
+++ 24.07/terceiro.py
@@ -0,0 +1,8 @@
+x = 10
+
+def mostrar():
+    y = 5
+    print(x, y)
+
+mostrar()
+print(y)
